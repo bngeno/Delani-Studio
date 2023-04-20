445ad196c5408176b31e70d038110e24c44bb06a
Benard Ng'eno <BNgeno@safaricom.co.ke>
2023-04-20 20:16:59 +0300

    icons

--- js/app.js
+++ js/app.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
-    $("#dev-image").click(function(){
-      $("#dev-image").slideDown('1500').hide('1000');
-      $("#dev").show('1500');
+    $("#development-image").click(function(){
+      $("#development-image").slideDown('1500').hide('1000');
+      $("#development").show('1500');
     });
-    $("#dev").click(function(){
-      $("#dev").slideUp('1500');
-      $("#dev-image").slideDown('1500');
+    $("#development").click(function(){
+      $("#development").slideUp('1500');
+      $("#development-image").slideDown('1500');
     });
   });
   
@@ -30,4 +30,4 @@ $(document).ready(function(){
       $("#product-image").slideDown('1500');
     });
   });
-  
+  s
